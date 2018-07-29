12c767d93c09889fe1e1a2a34383813f018a7409
Sheeka Patak <spatak@zendesk.com>
2018-07-29 14:22:45 +0100

    create image uploads

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_29_125430) do
+ActiveRecord::Schema.define(version: 2018_07_29_130943) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_file_name"
+    t.string "picture_content_type"
+    t.integer "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -24,6 +28,10 @@ ActiveRecord::Schema.define(version: 2018_07_29_125430) do
     t.integer "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_file_name"
+    t.string "picture_content_type"
+    t.integer "picture_file_size"
+    t.datetime "picture_updated_at"
     t.index ["city_id"], name: "index_reviews_on_city_id"
   end
 
